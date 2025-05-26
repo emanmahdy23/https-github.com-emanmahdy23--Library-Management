ab0f11e3486ea797ee6245df084acda9d5ca3c5e
emanmahdy23 <123274719+emanmahdy23@users.noreply.github.com>
2025-05-26 14:40:22 +0300

    Updated

--- Program.cs
+++ Program.cs
@@ -10,73 +10,72 @@ namespace project1
         {
             LibraryManagementAppContext context = new LibraryManagementAppContext();
 
-            var author = new Author { Name = "Agatha Christie" };
-            context.Authors.Add(author);
-            context.SaveChanges();
-            var books = new List<Book>
-            {
-                          new Book { Title = "Murder on the Orient Express", PublishedYear = 1934, AuthorId = 1 },
-                             new Book { Title = "And Then There Were None", PublishedYear = 1939, AuthorId = 1 }
-            };
+            //var author = new Author { Name = "Agatha Christie" };
+//context.Authors.Add(author);
+//context.SaveChanges();
+//            var books = new List<Book>
+//{
+//              new Book { Title = "Murder on the Orient Express", PublishedYear = 1934, AuthorId = 1 },
+//                 new Book { Title = "And Then There Were None", PublishedYear = 1939, AuthorId = 1 }
+//};
 
-            context.Books.AddRange(books);
-
-
-
-            var author1 = new Author { Name = "Arthur Conan Doyle" };
-            var book = new Book { Title = "Sherlock Holmes: A Study in Scarlet", PublishedYear = 1887, Author = author };
-
-            context.Books.Add(book);
-
-            var book1 = context.Books.FirstOrDefault(b => b.Id == 1);
-            if (book != null)
-            {
-                book.Title = "Updated Title";
-
-
-                context.SaveChanges();
+//            context.Books.AddRange(books);
 
 
 
-            }
+//            var author = new Author { Name = "Arthur Conan Doyle" };
+//            var book = new Book { Title = "Sherlock Holmes: A Study in Scarlet", PublishedYear = 1887, Author = author };
+
+//            context.Books.Add(book);
+
+//var book = context.Books.FirstOrDefault(b => b.Id == 1);
+//if (book != null)
+//{
+//    book.Title = "Updated Title";
+
+
+//    context.SaveChanges();
 
 
 
-            var member = new Member { Id = 2, FullName = "Eman mahdy" };
-            context.Entry(member).State = EntityState.Modified;
-            context.SaveChanges();
-            var booksByAuthor = context.Books.Where(b => b.AuthorId == 1).ToList();
-
-            var bookIds = booksByAuthor.Select(b => b.Id).ToList();
-
-            var relatedRecords = context.BookCheckouts.Where(bc => bookIds.Contains((int)bc.BookId)).ToList();
-
-            context.BookCheckouts.RemoveRange(relatedRecords);
-            context.Books.RemoveRange(booksByAuthor);
-            context.SaveChanges();
+//}
 
 
 
+//var member = new Member { Id = 2, FullName = "Eman mahdy" };
+//context.Entry(member).State = EntityState.Modified;
+//context.SaveChanges();
+//var booksByAuthor = context.Books.Where(b => b.AuthorId == 1).ToList();
 
-            var booksWithAuthors = context.Books.Include(b => b.Author).ToList();
-            foreach (var item in booksWithAuthors)
-            {
-                Console.WriteLine(item.Author.Name);
-            }
-            var book2 = context.Books.FirstOrDefault();
-            var authorName = book?.Author?.Name;
-            var books2 = context.Books.AsNoTracking().ToList();
+//var bookIds = booksByAuthor.Select(b => b.Id).ToList();
 
-            var book3 = new Book { Id = 2, Title = "Updated Title" };
-            context.Books.Attach(book);
-            context.Entry(book).Property(b => b.Title).IsModified = true;
-            context.SaveChanges();
+//var relatedRecords = context.BookCheckouts.Where(bc => bookIds.Contains((int)bc.BookId)).ToList();
+
+//context.BookCheckouts.RemoveRange(relatedRecords);
+//context.Books.RemoveRange(booksByAuthor);
+//context.SaveChanges();
 
 
-            int authorId = 2;
-            var books3 = context.Books.FromSqlRaw("EXEC GetBookByAuthorId @p0", authorId).ToList();
-            var books4 = context.Books.FromSqlInterpolated($"EXEC GetBookByAuthorId {authorId}").ToList();
 
+
+//var booksWithAuthors = context.Books.Include(b => b.Author).ToList();
+//foreach (var item in booksWithAuthors)
+//{
+//    Console.WriteLine(item.Author.Name);
+//}
+//var book = context.Books.FirstOrDefault();
+//var authorName = book?.Author?.Name;
+//var books = context.Books.AsNoTracking().ToList();
+
+//var book = new Book { Id = 2, Title = "Updated Title" };
+//context.Books.Attach(book);
+//context.Entry(book).Property(b => b.Title).IsModified = true;
+//context.SaveChanges();
+
+
+//int authorId = 2;
+//var books = context.Books.FromSqlRaw("EXEC GetBookByAuthorId @p0", authorId).ToList();
+//var books2 = context.Books.FromSqlInterpolated($"EXEC GetBookByAuthorId {authorId}").ToList();
             
         }
     }
